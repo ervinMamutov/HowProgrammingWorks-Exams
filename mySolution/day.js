9d4b6c117342d26591482794d6a40f17bb6b0664
Ervin Mamutov <ervin.news@gmail.com>
2024-03-03 21:44:21 +0100

    step thrid

---
+++ mySolution/day.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Get day number
+
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const parseDay = (s) => {
+  for (let i = 0; i < DAYS.length; i++) {
+    if (s.startsWith(DAYS[i].toLowerCase())) {
+      return ++i;
+    }
+  }
+  return -1;
+};
+
+require('../Tests/day.js')(parseDay);
