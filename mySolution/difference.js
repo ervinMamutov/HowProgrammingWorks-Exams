7bda3720042276dc013c35ac1fe03775cb0f6ab0
Ervin Mamutov <ervin.news@gmail.com>
2024-03-03 22:11:45 +0100

    step second

--- mySolution/difference.js
+++ mySolution/difference.js
@@ -2,6 +2,18 @@
 // Find a difference between two dictionaries
 
 const diff = (objectOne, objectTwo) => {
+  if (JSON.stringify(objectOne) === JSON.stringify(objectTwo)) return {};
+
+  const objectOneKeys = Object.keys(objectOne);
+  const objectTwoKeys = Object.keys(objectTwo);
+  let differentLength = 0;
+
+  if (objectOneKeys.length > objectTwoKeys.length) {
+    differentLength = objectOneKeys.length;
+  } else {
+    differentLength = objectTwoKeys.length;
+  }
+  console.log(objectOne, objectTwo);
   for (const attributeName in objectOne) {
     objectOne[attributeName] = objectOne[attributeName];
   }
@@ -10,6 +22,7 @@ const diff = (objectOne, objectTwo) => {
     objectOne[attributeName] = objectTwo[attributeName];
     delete objectOne[attributeName];
   }
+  console.log(objectOne);
   return objectOne;
 };
 
