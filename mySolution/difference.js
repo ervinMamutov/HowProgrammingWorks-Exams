9e3046f235700f085b11c4f1f330d3543de94fd1
Ervin Mamutov <ervin.news@gmail.com>
2024-03-04 10:00:04 +0100

    step fourth

--- mySolution/difference.js
+++ mySolution/difference.js
@@ -4,18 +4,6 @@
 const diff = (objectOne, objectTwo) => {
   if (JSON.stringify(objectOne) === JSON.stringify(objectTwo)) return {};
 
-  const objectOneKeys = Object.keys(objectOne);
-  const objectTwoKeys = Object.keys(objectTwo);
-  let differentLength = 0;
-
-  if (objectOneKeys.length > objectTwoKeys.length) {
-    differentLength = objectOneKeys.length;
-  } else {
-    differentLength = objectTwoKeys.length;
-  }
-  // for (const attributeName in objectOne) {
-  // objectOne[attributeName] = objectOne[attributeName];
-  // }
   console.log(objectOne, objectTwo);
   for (const attributeName in objectTwo) {
     delete objectOne[attributeName];
