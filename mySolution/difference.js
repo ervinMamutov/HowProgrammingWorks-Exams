2c069f04e6a188e1251574e93045d2b75ee0fcf1
Ervin Mamutov <ervin.news@gmail.com>
2024-03-04 10:01:09 +0100

    step fifth

--- mySolution/difference.js
+++ mySolution/difference.js
@@ -2,9 +2,6 @@
 // Find a difference between two dictionaries
 
 const diff = (objectOne, objectTwo) => {
-  if (JSON.stringify(objectOne) === JSON.stringify(objectTwo)) return {};
-
-  console.log(objectOne, objectTwo);
   for (const attributeName in objectTwo) {
     delete objectOne[attributeName];
   }
