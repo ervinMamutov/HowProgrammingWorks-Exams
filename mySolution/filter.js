06cbbd95c69e8954776d746693cc233fd16f5e52
Ervin Mamutov <ervin.news@gmail.com>
2024-03-08 15:27:25 +0100

    step 4

--- mySolution/filter.js
+++ mySolution/filter.js
@@ -2,15 +2,13 @@
 // Filter array by type name
 
 const filter = (array, type) => {
-  const remove = [];
-  for (const value of array) {
-    if (typeof value !== type) {
-      console.log('array ', array, 'value ', value);
-      console.log('index ', array.indexOf(value));
-      remove.unshift(array.indexOf(value));
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] !== type) {
+      array.splice(i, 1);
+      i--;
     }
   }
-  for (const x of remove) array.splice(x, 1);
+
   return array;
 };
 
