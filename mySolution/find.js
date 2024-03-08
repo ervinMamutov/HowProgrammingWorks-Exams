6c1701df86ab91a266852a9786867dbfdfa35df6
Ervin Mamutov <ervin.news@gmail.com>
2024-03-08 15:33:24 +0100

    step 1

---
+++ mySolution/find.js
@@ -0,0 +1,15 @@
+'use strict';
+// Find key by value
+
+const find = (object, ...rest) => {
+  const value = rest.pop(1);
+  for (const name in object) {
+    if (object[name] !== value) {
+    } else {
+      if (typeof name) return name;
+      if (typeof object) return object;
+    }
+  }
+};
+
+require('../Tests/find.js')(find);
