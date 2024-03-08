ba9fe4c08d66a0ea19e804f6a06a31936b8828aa
Ervin Mamutov <ervin.news@gmail.com>
2024-03-08 10:58:19 +0100

    step 3

--- mySolution/except.js
+++ mySolution/except.js
@@ -2,10 +2,8 @@
 // Copy all values from dict except listed
 
 const except = (incomingValuesArray, ...no) => {
-  console.log(incomingValuesArray, no);
   const intermediateVariable = Object.keys(incomingValuesArray);
   intermediateVariable.forEach((variable) => {
-    [].sort(() => 2000);
     if (no.includes(variable)) {
       delete incomingValuesArray[variable];
       return;
