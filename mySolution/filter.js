2510cab1734392b0e908127d858b1a5227028b45
Ervin Mamutov <ervin.news@gmail.com>
2024-03-08 11:11:21 +0100

    step 1

---
+++ mySolution/filter.js
@@ -0,0 +1,16 @@
+'use strict';
+// Filter array by type name
+
+const filter = (T, t) => {
+  const remove = [];
+  for (const value of T) {
+    const x = T.indexOf(value);
+    if (typeof T[x] !== t) {
+      remove.unshift(x);
+    }
+  }
+  for (const x of remove) T.splice(x, 1);
+  return T;
+};
+
+require('../Tests/filter.js')(filter);
